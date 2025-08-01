6707badce3b6273bec27b05dfd8fb152cd50da21
Dorian <daguisaoo@eafit.edu.co>
2025-07-31 20:33:34 -0500

    visualización de películas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 
 # Create your views here.
@@ -7,8 +8,16 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to home page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': ' Dorian Alejandro Guisao Ospina'})
+    #return render(request, 'home.html', {'name': ' Dorian Alejandro Guisao Ospina'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
 
 def about(request):
-    return HttpResponse('<h1>Welcome to about</h1>')
+    #return HttpResponse('<h1>Welcome to about</h1>')
+    return render(request, 'about.html')
     
